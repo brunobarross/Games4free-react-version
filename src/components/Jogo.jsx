23270ef0adc843f772a1e5f628f0bcd8260f3168
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2022-07-08 18:44:52 -0300

    atualizando

--- src/components/Jogo.jsx
+++ src/components/Jogo.jsx
@@ -9,6 +9,7 @@ const Jogo = ({
   sidebarOpen,
   setSideBarOpen,
 }) => {
+  const regex = /\d+/g;
   const [jogo, setJogo] = useState('');
   let { id } = useParams();
   const getData = async (url) => {
@@ -38,56 +39,43 @@ const Jogo = ({
 
   return (
     <div
-      className={`mx-auto w-full sm:pl-[300px] relative min-h-screen overflow-hidden flex ${
+      className={`bg-white mx-auto w-full sm:pl-[300px] py-16 justify-center relative min-h-screen overflow-hidden flex ${
         isLoading ? 'justify-center items-center' : ''
       }`}
     >
       <Navbar sidebarOpen={sidebarOpen} setSideBarOpen={setSideBarOpen} />
       {jogo && (
-        <div className="card-jogo flex flex-col h-full">
-          <div className="foto">
-            <img src={jogo.image} className="w-full max-h-60" />
-          </div>
-          <div className="header ">
-            <div className="header-item">
-              <h3>Platform:</h3>
-              <span
-                className={`badge  ${jogo.platforms
-                  .toLowerCase()
-                  .split(',')[0]
-                  .replace(/ /g, '-')
-                  .replace('|', '-')}`}
-              >
-                {jogo.platforms.split(',')[0]}
-              </span>
-            </div>
-
-            <div className="header-item">
-              <h3>Users:</h3>
-              <span className="badge resgates w-max">{jogo.users}</span>
-            </div>
-            <div className="header-item">
-              <h3>Value:</h3>
-              <span>FREE</span>
-              <span className="badge valor line-through">{jogo.worth}</span>
-            </div>
-            <div className="header-item">
-              <h3>Type:</h3>
-
-              <span className="tipo w-max">{jogo.type}</span>
-            </div>
+        <div className="card-jogo ">
+          <div className="header">
+            <img src={jogo.image} className="w-full h-full max-h-[300px]" />
           </div>
           <div className="informacoes">
             <div className="titulo">
               <h3>{jogo.title}</h3>
             </div>
+            <div className="plataforma">
+              <p>
+                {jogo.type} |{' '}
+                <span className="badge inline-flex">
+                  {jogo.platforms.split(',')[0]}
+                </span>
+                <span className="badge inline-flex ml-2">
+                  {jogo.platforms.split(',')[1]}
+                </span>
+              </p>
+            </div>
+            <div className="valor">
+              <p>
+                FREE <span>{jogo.worth}</span>
+              </p>
+            </div>
             <div className="descricao">
               <p>{jogo.description}</p>
             </div>
-          </div>
-          <div className="instrucoes">
-            <h3>How to get the game</h3>
-            <p>{jogo.instructions}</p>
+            <div className="instrucoes">
+              <h3>How to get the game</h3>
+              <p>{jogo.instructions}</p>
+            </div>
           </div>
         </div>
       )}
